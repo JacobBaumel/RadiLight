c9170f94d328a165796e261204b283ae3d4bba10
Karthik Duvvuri <karthik.duvvuri08@gmail.com>
2024-08-11 19:46:40 -0500

    hard coded in some things plus debugging

--- main.cu
+++ main.cu
@@ -3,7 +3,6 @@
 #include <opencv2/opencv.hpp>
 #include <chrono>
 #include "nvapriltags/include/nvAprilTags.h"
-using namespace cv;
 
 // copy from https://github.com/NVIDIA-AI-IOT/ros2-nvapriltags/blob/main/src/AprilTagNode.cpp
 
@@ -33,12 +32,11 @@ struct AprilTagsImpl {
     void initialize(const uint32_t width,
                     const uint32_t height, const size_t image_buffer_size,
                     const size_t pitch_bytes,
-                    const float fx, const float fy, const float cx, const float cy,
                     float tag_edge_size_, int max_tags_) {
         assert(!april_tags_handle), "Already initialized.";
 
         // Get camera intrinsics
-        cam_intrinsics = {fx, fy, cx, cy};
+        cam_intrinsics = {802.9265702293416, 803.6309422064642, 966.4221440154661, 477.6409613889424};
 
         // Create AprilTags detector instance and get handle
         const int error = nvCreateAprilTagsDetector(
@@ -83,35 +81,25 @@ struct AprilTagsImpl {
 
 int main() {
     printf("cuda main");
-    VideoCapture capture;
-    int width = 1920;
-    int height = 1080;
-    float fx = 802.9265702293416;
-    float fy = 803.6309422064642;
-    float cx = 966.4221440154661;
-    float cy = 477.6409613889424;
-
-
+    cv::VideoCapture capture;
     capture.open(0);
-    capture.set(cv::CAP_PROP_FRAME_WIDTH, width);
-    capture.set(cv::CAP_PROP_FRAME_HEIGHT, height);
-    Mat frame;
-    Mat img_rgba8;
-    capture>>frame;
+    capture.set(cv::CAP_PROP_FPS, 30);
+    //capture.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    //capture.set(cv::CAP_PROP_FRAME_WIDTH, 1920);
+    //capture.set(cv::CAP_PROP_FRAME_HEIGHT, 1080);
+    cv::Mat frame;
+    cv::Mat img_rgba8;
+    capture >> frame;
     cv::cvtColor(frame, img_rgba8, cv::COLOR_BGR2RGBA);
     auto *impl_ = new AprilTagsImpl();
     impl_->initialize(img_rgba8.cols, img_rgba8.rows,
-                      img_rgba8.total() * img_rgba8.elemSize(),  img_rgba8.step,
-                      fx,fy,cx,cy,
-                      0.5,
-                      6);
+                      img_rgba8.total() * img_rgba8.elemSize(),  img_rgba8.step, .1651f, 2);
 
-    while (capture.isOpened())
-    {
-
-        capture>>frame;
-        cv::cvtColor(frame, img_rgba8, cv::COLOR_BGR2RGBA);
+    while (capture.isOpened()){
+        capture >> frame;
         auto start = std::chrono::system_clock::now();
+        cv::cvtColor(frame, img_rgba8, cv::COLOR_BGR2RGBA);
+        
 
         const cudaError_t cuda_error =
                 cudaMemcpy(impl_->input_image_buffer, (uchar4 *)img_rgba8.ptr<unsigned char>(0),
@@ -131,23 +119,18 @@ int main() {
             throw std::runtime_error("Failed to run AprilTags detector (error code " +
                                      std::to_string(error) + ")");
         }
-
-        for (int i = 0; i < num_detections; i++) {
-            const nvAprilTagsID_t &detection = impl_->tags[i];
-
-            // corners
-            for (auto corner : detection.corners) {
-               float x = corner.x;
-               float y = corner.y;
-            }
-
-        }
-
         auto end = std::chrono::system_clock::now();
-        int fps = int(1000 / ( std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() + 1));
-        cv::putText(frame, "FPS: "+ std::to_string(fps), cv::Point(100,100),
-                    cv::FONT_HERSHEY_PLAIN, 5, cv::Scalar(0xFF, 0xFF, 0), 2);
-        std::cout<<"num_detections: "<<num_detections<<std::endl;
+        for (int i = 0; i < num_detections; i++) {
+            const nvAprilTagsID_t &detection = impl_->tags[i];       
+        }
+        int latency = int(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+        //int fps = int(1000 / ( std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() + 1));
+      //  std::cout << fps << std::endl;
+        std::cout << latency << std::endl;
+        //std::cout << capture.get(cv::CAP_PROP_FRAME_COUNT) << std::endl;
+        //std::cout << capture.get(cv::CAP_PROP_FRAME_WIDTH) << std::endl;
+        //std::cout << capture.get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl;
+        //std::cout << capture.get(cv::CAP_PROP_FOURCC) << std::endl;
 
         cv::namedWindow("frame", 0);
         cv::resizeWindow("frame", 1280,800);
