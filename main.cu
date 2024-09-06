b9f67be3ca986b4b0af911b3a7f54b512f629c84
Karthik Duvvuri <karthik.duvvuri08@gmail.com>
2024-09-05 21:38:32 -0500

    MultiTag Implemented (need to test)

--- main.cu
+++ main.cu
@@ -2,6 +2,7 @@
 #include "cuda_runtime.h"
 #include <opencv2/opencv.hpp>
 #include <opencv2/cudaimgproc.hpp>
+#include <opencv2/calib3d.hpp>
 #include "nvapriltags/include/nvAprilTags.h"
 #include <iostream>
 #include <string.h>
@@ -88,13 +89,13 @@ struct AprilTagsImpl {
     }
 };
 
-
-float *converterQuaternion(const float *matrix ){
-    float *quaternion = new float[4];
-    float fourWSquaredMinus1 = matrix[0] + matrix[4] + matrix[8];
-            float fourXSquaredMinus1 = matrix[0] - matrix[4] - matrix[8];
-            float fourYSquaredMinus1 = matrix[4] - matrix[0] - matrix[8];
-            float fourZSquaredMinus1 = matrix[8] - matrix[0] - matrix[4];
+/*
+std::array<float, 4>converterQuaternion(const cv::Mat& matrix){
+    std::array<float, 4> quaternion;
+    float fourWSquaredMinus1 = matrix.at<float>(0, 0) + matrix.at<float>(1, 1) + matrix.at<float>(2, 2);
+    float fourXSquaredMinus1 = matrix.at<float>(0, 0) - matrix.at<float>(1, 1) - matrix.at<float>(2, 2);
+    float fourYSquaredMinus1 = matrix.at<float>(1, 1) - matrix.at<float>(0, 0) - matrix.at<float>(2, 2);
+    float fourZSquaredMinus1 = matrix.at<float>(2, 2) - matrix.at<float>(0, 0) - matrix.at<float>(1, 1);
 
             int biggestIndex = 0;
             float fourBiggestSquaredMinus1 = fourWSquaredMinus1;
@@ -111,72 +112,104 @@ float *converterQuaternion(const float *matrix ){
                 fourBiggestSquaredMinus1 = fourZSquaredMinus1;
                 biggestIndex = 3;
             }
-            // Per form square root and division
+            
             float biggestVal = sqrt (fourBiggestSquaredMinus1 + 1.0f ) * 0.5f;
             float mult = 0.25f / biggestVal;
+            
+    switch (biggestIndex) {
+        case 0:
+            quaternion[0] = biggestVal;
+            quaternion[1] = (matrix.at<float>(2, 1) - matrix.at<float>(1, 2)) * mult;
+            quaternion[2] = (matrix.at<float>(0, 2) - matrix.at<float>(2, 0)) * mult;
+            quaternion[3] = (matrix.at<float>(1, 0) - matrix.at<float>(0, 1)) * mult;
+            break;
+        case 1:
+            quaternion[1] = biggestVal;
+            quaternion[0] = (matrix.at<float>(2, 1) - matrix.at<float>(1, 2)) * mult;
+            quaternion[2] = (matrix.at<float>(1, 0) + matrix.at<float>(0, 1)) * mult;
+            quaternion[3] = (matrix.at<float>(0, 2) + matrix.at<float>(2, 0)) * mult;
+            break;
+        case 2:
+            quaternion[2] = biggestVal;
+            quaternion[0] = (matrix.at<float>(0, 2) - matrix.at<float>(2, 0)) * mult;
+            quaternion[1] = (matrix.at<float>(1, 0) + matrix.at<float>(0, 1)) * mult;
+            quaternion[3] = (matrix.at<float>(2, 1) + matrix.at<float>(1, 2)) * mult;
+            break;
+        case 3:
+            quaternion[3] = biggestVal;
+            quaternion[0] = (matrix.at<float>(1, 0) - matrix.at<float>(0, 1)) * mult;
+            quaternion[1] = (matrix.at<float>(0, 2) + matrix.at<float>(2, 0)) * mult;
+            quaternion[2] = (matrix.at<float>(2, 1) + matrix.at<float>(1, 2)) * mult;
+            break;
+    }
 
-            // Apply table to compute quaternion values
-            switch (biggestIndex) {
-                case 0:
-                    quaternion[0] = biggestVal;
-                    quaternion[1] = (matrix[7] - matrix[5]) * mult;
-                    quaternion[2] = (matrix[2] - matrix[6]) * mult;
-                    quaternion[3] = (matrix[3] - matrix[1]) * mult;
-                    break;
-                case 1:
-                    quaternion[1] = biggestVal;
-                    quaternion[0] = (matrix[7] - matrix[5]) * mult;
-                    quaternion[2] = (matrix[3] + matrix[1]) * mult;
-                    quaternion[3] = (matrix[2] + matrix[6]) * mult;
-                    break;
-                case 2:
-                    quaternion[2] = biggestVal;
-                    quaternion[0] = (matrix[2] - matrix[6]) * mult;
-                    quaternion[1] = (matrix[3] + matrix[1]) * mult;
-                    quaternion[3] = (matrix[7] + matrix[5]) * mult;
-                    break;
-                case 3:
-                    quaternion[3] = biggestVal;
-                    quaternion[0] = (matrix[3] - matrix[1]) * mult;
-                    quaternion[1] = (matrix[2] + matrix[6]) * mult;
-                    quaternion[2] = (matrix[7] + matrix[5]) * mult;
-                    break;
-            }
-            return quaternion;
+    return quaternion;
+}*/
+
+std::array<float, 4> averageRotationMatrixToQuaternion(const std::vector<cv::Mat>& rotationMatrices) {
+    cv::Mat avgRotationMatrix = cv::Mat::zeros(3, 3, CV_64F);
+
+    for (const auto& R : rotationMatrices) {
+        avgRotationMatrix += R;
+    }
+
+    avgRotationMatrix /= rotationMatrices.size();
+
+    cv::Mat U, S, VT;
+    cv::SVD::compute(avgRotationMatrix, S, U, VT);
+    avgRotationMatrix = U * VT;
+
+    cv::Mat quaternion;
+    avgRotationMatrix.convertTo(avgRotationMatrix, CV_32F);
+    cv::Rodrigues(avgRotationMatrix, quaternion);
+
+    float norm = cv::norm(quaternion);
+    quaternion /= norm;
+
+    std::array<float, 4> quatArray;
+    quatArray[0] = quaternion.at<float>(0); // x
+    quatArray[1] = quaternion.at<float>(1); // y
+    quatArray[2] = quaternion.at<float>(2); // z
+    quatArray[3] = quaternion.at<float>(3); // w
+
+    return quatArray;
 }
 
-void roborioSender(float sendData[7], size_t size, int sock) {
+void roborioSender(std::array<float, 7>& sendData, int sock) {
     sockaddr_in destination;
     destination.sin_family = AF_INET;
     destination.sin_port = htons(9000);
     destination.sin_addr.s_addr = inet_addr("192.168.86.42");
 
-    int n_bytes = ::sendto(sock, sendData, size, 0, reinterpret_cast<sockaddr*>(&destination), sizeof(destination));
+    size_t size = sendData.size() * sizeof(float);
+
+    int n_bytes = ::sendto(sock, sendData.data(), size, 0, reinterpret_cast<sockaddr*>(&destination), sizeof(destination));
     std::cout << n_bytes << " bytes sent" << std::endl;
 }
 
-void webserverSender(float sendData[7], size_t size, int sock) {
+void webserverSender(std::array<float, 7>& sendData, int sock) {
     sockaddr_in destination;
     destination.sin_family = AF_INET;
     destination.sin_port = htons(9001);
     destination.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    int n_bytes = ::sendto(sock, sendData, size, 0, reinterpret_cast<sockaddr*>(&destination), sizeof(destination));
+    size_t size = sendData.size() * sizeof(float);
+
+    int n_bytes = ::sendto(sock, sendData.data(), size, 0, reinterpret_cast<sockaddr*>(&destination), sizeof(destination));
     std::cout << n_bytes << " bytes sent" << std::endl;
 }
 
-char *webserverRecevier(int sock) {
+std::string webserverRecevier(int sock) {
     int len = sizeof(sockaddr_in);
-    char *buff = new char[1024];
+    char buff[1024];
     sockaddr_in destination;
     destination.sin_family = AF_INET;
     destination.sin_port = htons(9001);
     destination.sin_addr.s_addr = inet_addr("127.0.0.1");
-    
 
-    int readStatus = recvfrom(sock, buff, 1024, 0, (struct sockaddr*)&destination, (socklen_t*)&len);
+    int readStatus = recvfrom(sock, buff, sizeof(buff), 0, (struct sockaddr*)&destination, (socklen_t*)&len);
     buff[readStatus] = '\0';
-    return buff;
+    return std::string(buff);
 }
     
 void setStaticIP(char ip_address[15]){
@@ -221,20 +254,76 @@ void setStaticIP(char ip_address[15]){
     std::cout << ("IP Address updated sucessfully.\n");
 }
 
+std::array<float, 7> multiTagFinder(uint32_t numDetections, const std::vector<std::vector<cv::Point2d>> &imagePtsSet){
+    static std::array<float, 7> lastValidPose = {0}; 
+    std::array<float, 7> finalPose = {0};
+
+    if (numDetections == 0 || imagePtsSet.empty()) {
+        std::cout << "No detections" << "\n";
+        return lastValidPose;
+    }
+    
+    std::vector<cv::Point3d> apriltagPts = {
+        cv::Point3d(-0.08255, 0.08255, 0), 
+        cv::Point3d(0.08255, 0.08255, 0), 
+        cv::Point3d(0.08255, -0.08255, 0), 
+        cv::Point3d(-0.08255, -0.08255, 0)
+    }; 
+
+    float K[] = {802.9265702293416, 0, 966.4221440154661, 0, 803.6309422064642, 477.6409613889424, 0, 0, 1};
+    cv::Mat kMat(3, 3, CV_32F, K);
+
+    float distCoeffs[5] = {0}; 
+    cv::Mat distMat(1, 5, CV_32F, distCoeffs);
+
+    cv::Mat avgTranslation = cv::Mat::zeros(3, 1, CV_64F);
+
+    std::vector<cv::Mat> rotationMatrices;
+
+    for (uint32_t i = 0; i < numDetections; i++) {
+        std::vector<cv::Point2d> imagePts = imagePtsSet[i];
+        imagePts = {imagePts[1], imagePts[2], imagePts[3], imagePts[0]};
+        cv::Mat rvec, tvec, R;
+
+        bool error = cv::solvePnP(apriltagPts, imagePts, kMat, distMat, rvec, tvec, false, cv::SOLVEPNP_IPPE_SQUARE);
+        if (!error){
+            std::cout << "Error with SolvePNP" << "\n";
+            return lastValidPose;
+        }
+        cv::Rodrigues(rvec, R);
+        rotationMatrices.push_back(R);
+        avgTranslation += tvec;
+    }
+    avgTranslation /= numDetections;
+
+    std::array<float, 4> avgQuaternion = averageRotationMatrixToQuaternion(rotationMatrices);
+
+    finalPose[0] = static_cast<float>(avgTranslation.at<double>(0));
+    finalPose[1] = static_cast<float>(avgTranslation.at<double>(1));
+    finalPose[2] = static_cast<float>(avgTranslation.at<double>(2));
+    finalPose[3] = avgQuaternion[0];
+    finalPose[4] = avgQuaternion[1];
+    finalPose[5] = avgQuaternion[2];
+    finalPose[6] = avgQuaternion[3];
+    
+    lastValidPose = finalPose;
+    return finalPose;
+}
+
+
 int main() {
-    setStaticIP("192.168.86.5");
+    //setStaticIP("192.168.86.5");
     printf("cuda main");
-    float *quaternion;
-    char *buff;
-    float sendData[7];
+    std::array<float, 7> finalPose;
+    std::vector<cv::Point2d> apriltagPoints;
+    std::vector<std::vector<cv::Point2d>> imagePts;
+    std::string buff;
     int roborioSock = ::socket(AF_INET, SOCK_DGRAM, 0);
     int webserverSock = ::socket(AF_INET, SOCK_DGRAM, 0);
-
     cv::VideoCapture capture = cv::VideoCapture(0, cv::CAP_GSTREAMER);
     cv::Mat frame;
     cv::Mat img_rgba8;
     cv::cuda::GpuMat img_rgba8gpu;  
-    capture.open(0);
     //capture.set(cv::CAP_PROP_FPS, 30);
     capture.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
     capture.set(cv::CAP_PROP_FRAME_WIDTH, 1920);
@@ -247,7 +336,7 @@ int main() {
     img_rgba8gpu.download(img_rgba8);
     auto *impl_ = new AprilTagsImpl();
     impl_->initialize(img_rgba8.cols, img_rgba8.rows,
-                      img_rgba8.total() * img_rgba8.elemSize(),  img_rgba8.step, .1651f, 1);
+                      img_rgba8.total() * img_rgba8.elemSize(),  img_rgba8.step, .1651f, 5);
 
     while (capture.isOpened()){
         //auto start = std::chrono::system_clock::now();
@@ -266,46 +355,41 @@ int main() {
                     std::to_string(cuda_error) + ")");
         }
 
-        uint32_t num_detections;
+        uint32_t numDetections;
         const int error = nvAprilTagsDetect(
                 impl_->april_tags_handle, &(impl_->input_image), impl_->tags.data(),
-                &num_detections, impl_->max_tags, impl_->main_stream);
+                &numDetections, impl_->max_tags, impl_->main_stream);
         if (error != 0) {
             throw std::runtime_error("Failed to run AprilTags detector (error code " +
                                      std::to_string(error) + ")");
         }
-            
-        for (int i = 0; i < num_detections; i++) {
-            const nvAprilTagsID_t &detection = impl_->tags[i]; 
-            quaternion = converterQuaternion(detection.orientation);
-            sendData[0] = detection.translation[0];
-            sendData[1] = detection.translation[1];
-            sendData[2] = detection.translation[2];
-            sendData[3] = quaternion[0];
-            sendData[4] = quaternion[1];
-            sendData[5] = quaternion[2];
-            sendData[6] = quaternion[3];
-            webserverSender(sendData, sizeof(sendData), webserverSock);
-            roborioSender(sendData, sizeof(sendData), roborioSock);
-            if(webserverRecevier(webserverSock))
-            buff = webserverRecevier(webserverSock);
+
+        imagePts.clear(); 
+        apriltagPoints.clear();
+        for (int i = 0; i < numDetections; i++) {
+            const nvAprilTagsID_t &detection = impl_->tags[i];
             for (auto corner : detection.corners) {
-                float x = corner.x;
-                float y = corner.y;
-                cv::circle(frame, cv::Point(x, y), 4, cv::Scalar(255, 0, 0), -1);
+                apriltagPoints.push_back(cv::Point2d(corner.x, corner.y));
+                cv::circle(frame, cv::Point(corner.x, corner.y), 4, cv::Scalar(255, 0, 0), -1);
             }
+            imagePts.push_back(apriltagPoints);
         }
+
+        finalPose = multiTagFinder(numDetections, imagePts);
+        for (int i = 0; i < finalPose.size(); i++){
+            std::cout << finalPose[i] << "\n";
+        }    
+        webserverSender(finalPose, webserverSock);
+        roborioSender(finalPose, roborioSock);
+        buff = webserverRecevier(webserverSock);
         std::cout << buff;
-        //auto end = std::chrono::system_clock::now();
         cv::imshow("frame", frame); 
-        //std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
         if (cv::waitKey(10)==27)
-             break;
+                break;
     }
-    delete[] quaternion;
-    delete[] buff;
+
     ::close(roborioSock);
     ::close(webserverSock);
-    delete(impl_);
+    delete impl_;
     return 0;
 }
